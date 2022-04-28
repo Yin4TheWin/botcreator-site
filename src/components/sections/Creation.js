78162aa32f3150c573b785d827a75916422f8a95
Franklin Yin <yin4thewin@gmail.com>
2022-04-28 04:09:09 -0400

    Remove unnecessary client id field in dashboard

---
+++ src/components/sections/Creation.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+const axios = require('axios').default;
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+const Creation = () => {
+    const [open, setOpen] = React.useState(false);
+    const [name, setName] = React.useState("")
+    const [token, setToken] = React.useState("")
+    const [modalTitle, setModalTitle] = React.useState("")
+    const [modalText, setModalText] = React.useState("")
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const handleSuccess = (projName) => {
+        setModalTitle("Bot Created 🥳")
+        setModalText("Congratulations, "+projName+" has been created! Now go to a server with your bot in it and use /help to get to know your new creation!")
+        setName("")
+        setToken("")
+        handleOpen()
+    }
+    const handleFailure = (msg) => {
+        setModalTitle("Something Went Wrong ☹️")
+        setModalText(msg)
+        handleOpen()
+    }
+    return (
+        <div style={{display: 'flex',  justifyContent:'center', flexDirection: 'column', alignItems:'center', height: 'auto', width: 'auto', borderRadius: '15px', backgroundColor: '#498387', paddingLeft: '3vw', paddingRight: '3vw'}}>
+            <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2" color="black">
+                        {modalTitle}
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }} color="black">
+                        {modalText}
+                    </Typography>
+                </Box>
+            </Modal>
+            <h1> Create a new bot </h1>
+            <p onClick={()=>{
+                setModalTitle("Setup Instructions")
+                setModalText(`As you'll soon see, getting your own custom Discord bot is really simple! Just fill out the two fields you see in the box. Really quickly, I'll explain what they are and how to find them: ${"\n"}
+                (insert video here)${"\n"}`)
+                handleOpen()
+            }} style={{marginBottom: '2vh', color: 'blue', textDecorationLine: 'underline'}}>(Need help?)</p>
+            <TextField fullWidth style={{paddingBottom: '3vh'}} value={name} onChange={(e)=>{setName(e.target.value)}} label="Project Name" variant="outlined" color="secondary"/>
+            <TextField fullWidth style={{paddingBottom: '3vh'}} value={token} onChange={(e)=>{setToken(e.target.value)}} label="Bot Token" variant="outlined" color="warning" type="password"/>
+            <Button onClick={()=>{
+                 axios.post('http://137.184.143.181:3000/birth', {botToken: token, projectName: name, username: "yinftw"})
+                     .then(() => {
+                         let projName=name
+                         handleSuccess(projName)
+                        }
+                     ).catch(err=>{
+                        if(name===""||token===""){
+                            handleFailure("None of the fields may be blank!")
+                        } else{
+                            handleFailure("Invalid token or project already exists.")
+                        }
+                     })
+            }} style={{marginBottom: '3vh'}} variant="contained">Create!</Button>
+        </div>
+    );
+}
+
+export default Creation;
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,24 +1,14 @@
 import React from 'react';
-// import sections
-import Hero from '../components/sections/Hero';
-import FeaturesTiles from '../components/sections/FeaturesTiles';
-import FeaturesSplit from '../components/sections/FeaturesSplit';
-import Testimonial from '../components/sections/Testimonial';
-import Cta from '../components/sections/Cta';
-import TextField from '@mui/material/TextField';
+import Creation from '../components/sections/Creation'
 
-const Home = () => {
+
+const Dashboard = () => {
 
   return (
     <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-        <div style={{display: 'flex',  justifyContent:'center', flexDirection: 'column', alignItems:'center', height: 'auto', width: 'auto', borderRadius: '5px', backgroundColor: '#498387', paddingLeft: '3vw', paddingRight: '3vw'}}>
-            <h1> Create a new bot </h1>
-            <TextField fullWidth style={{paddingBottom: '3vh'}} label="Bot Name" variant="outlined" color="secondary"/>
-            <TextField fullWidth style={{paddingBottom: '3vh'}} label="Client ID" variant="outlined" color="success"/>
-            <TextField fullWidth style={{paddingBottom: '3vh'}} label="Bot Token" variant="outlined" color="warning"/>
-        </div>
+        <Creation/>
     </div>
   );
 }
 
-export default Home;
+export default Dashboard;
